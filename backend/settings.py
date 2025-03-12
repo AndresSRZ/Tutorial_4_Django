d2a70b407b4e06f72cba25d60785433d4c9fda65
Andres <asuarezr2@eafit.edu.co>
2025-03-11 21:38:58 -0500

    Final_Commit

--- backend/settings.py
+++ backend/settings.py
@@ -37,8 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    # Custom apps start here
+    # Custom Apps
     'todo.apps.TodoConfig',
+    'api.apps.ApiConfig',
+    # Third-party Apps
+    'rest_framework',
+    'rest_framework.authtoken',
 ]
 
 MIDDLEWARE = [
